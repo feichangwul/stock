1bf403baac86e4bce43b036176c3ba6b97c3e217
ibm <13817197890_jin@sina.com>
2016-10-06 23:17:54 +0800

    add layout

---
+++ StockRoom/StockRoom.BLL/Controller/LayoutController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using wojilu;
+using wojilu.Web.Mvc;
+
+namespace StockRoom.BLL.Controller
+{
+
+    public class LayoutController : ControllerBase {
+
+        private static readonly ILog logger = LogManager.GetLogger( typeof( LayoutController ) );
+
+        public LayoutController() {
+        }
+
+        public override void Layout() {
+
+            logger.Info( "开始加载布局文件" );
+
+            
+
+        }
+
+
+    }
+}
